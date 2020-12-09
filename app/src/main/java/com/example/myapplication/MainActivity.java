fa7ebe56804313db4287068910796de40ecd9aed
dam <damien.chancerel@gmail.com>
2020-12-09 11:34:17 +0100

    test2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //voir dans le xml activity_main.xml "button1:onClick"
+    public void clickButton1(View v){
+        Toast t = Toast.makeText(getApplicationContext(),
+                "Bouton 1",
+                Toast.LENGTH_SHORT);
+        t.show();
     }
 }
