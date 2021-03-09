dbacd0c77a06a47e9ba1acbce366efc0c66a3344
juju773 <ju.li1@hotmail.fr>
2021-03-09 18:43:30 +0100

    test de connexion à la bd

--- app/src/main/java/com/example/timelapse/GalerieFragment.java
+++ app/src/main/java/com/example/timelapse/GalerieFragment.java
@@ -1,20 +1,46 @@
 package com.example.timelapse;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
-public class GalerieFragment extends Fragment{
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+
+public class GalerieFragment extends Fragment implements View.OnClickListener{
 
     View root;
+    TextView texte;
+    Button b;
+    ArrayList<Integer> temperature = new ArrayList<Integer>();
+
 
 
     @Nullable
@@ -23,15 +49,37 @@ public class GalerieFragment extends Fragment{
         root =  inflater.inflate(R.layout.fragment_galerie, container, false);
         ImageButton Reglage = (ImageButton) root.findViewById(R.id.reglage_button);
 
-        Reglage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentTransaction fr = getFragmentManager().beginTransaction();
-                fr.replace(R.id.fragment_container, new ReglagesPage());
-                fr.commit();
-            }
-        });
+        texte = root.findViewById(R.id.textViewSQL);
+        b = root.findViewById(R.id.buttonSQL);
+        b.setOnClickListener(this);
+
+
         return root;
     }
 
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.buttonSQL:
+                RequestQueue queue = Volley.newRequestQueue(getContext());
+                String myurl= "http://91.174.199.48:49154";
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, myurl,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                Toast.makeText(getContext(), response.toString(), Toast.LENGTH_LONG).show();
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getContext(), "nope", Toast.LENGTH_LONG).show();
+                    }
+                });
+                queue.add(stringRequest);
+                queue.start();
+                break;
+        }
+    }
 }
+
+
